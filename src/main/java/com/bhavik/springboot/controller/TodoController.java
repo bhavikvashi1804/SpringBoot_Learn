9cb4ed7072e508cc14db342a926aa4bb62a06d14
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-02-23 18:23:38 +0530

    Bind the Date correctly

--- src/main/java/com/bhavik/springboot/controller/TodoController.java
+++ src/main/java/com/bhavik/springboot/controller/TodoController.java
@@ -1,13 +1,17 @@
 package com.bhavik.springboot.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +28,12 @@ public class TodoController {
 	@Autowired
 	TodoService service;
 	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, false));
+	}
 
 	@RequestMapping(value="/list-todos",method=RequestMethod.GET)
 	public String showLoginPage(ModelMap map) {
