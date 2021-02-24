a71bd05d2bfbaf4084f949744bd1dc0237b0e47e
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-02-24 11:21:44 +0530

    Remove the old login functionality

--- src/main/java/com/bhavik/springboot/controller/LoginController.java
+++ src/main/java/com/bhavik/springboot/controller/LoginController.java
@@ -13,27 +13,14 @@ import com.bhavik.springboot.service.LoginService;
 @Controller
 @SessionAttributes("name")
 public class LoginController {
-	
-	
+
 	@Autowired
 	LoginService loginService;
 
-	@RequestMapping(value="/login",method=RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showLoginPage(ModelMap map) {
-		return "login";
+		map.put("name", "bhavik");
+		return "welcome";
 	}
-	
-	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public String showWelcomePage(ModelMap map, @RequestParam String name,@RequestParam String password) {
-		
-		boolean isValidUser = loginService.validateUser(name, password);
-		if(isValidUser) {
-			map.put("name", name);
-			return "welcome";
-		}
-		else {
-			map.put("msg", "Invalid Password");
-			return "login";
-		}
-	}
+
 }
