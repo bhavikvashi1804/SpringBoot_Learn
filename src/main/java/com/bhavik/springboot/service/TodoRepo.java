3355ad70352fc0507398252b49b793958eb8105b
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-02-25 15:33:21 +0530

    Create a ToDo Repo

---
+++ src/main/java/com/bhavik/springboot/service/TodoRepo.java
@@ -0,0 +1,16 @@
+package com.bhavik.springboot.service;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bhavik.springboot.model.Todo;
+
+public interface TodoRepo extends JpaRepository<Todo, Integer> {
+	List<Todo> findByUser(String userName);
+	/*
+	 * deleteTodo - Use repository.deleteById(id); instead of repository.delete(id);
+	 * showUpdateTodoPage - Use Todo todo = repository.findById(id).get(); instead
+	 * of Todo todo = repository.findOne(id);
+	 */
+}
